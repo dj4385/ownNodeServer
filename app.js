d93a842b666849f49252ef5f9893bb412226e6c9
dj4385 <dj4385@gmail.com>
2019-09-01 19:02:07 +0530

    add the href to change the working flow of app

--- app.js
+++ app.js
@@ -9,10 +9,9 @@ const http = require('http'),
 function serveStaticFile(req,res){
     if(req.url == '/'){
         serveFiles('/register.html', res)
-    } else 
-    if(isStatic(req.url)){
+    } else if(isStatic(req.url)){
         serveFiles(req.url,res)
-    } if(req.url === '/doRegister' && req.method == 'POST'){
+    } else if(req.url === '/doRegister' && req.method == 'POST'){
         var data = ''
         req.on('data',chunk=>{
             data += chunk
@@ -27,8 +26,8 @@ function serveStaticFile(req,res){
                 userDb.userData.forEach(ele=>{
                     console.log('Array is',ele)
                 })
-                
-                res.write('User Register Successfully' )
+                res.setHeader('Content-type','text/html')
+                res.write('User Register Successfully. <a href="login.html">Login Now</a>' )
                 res.end()
             }
         })
@@ -37,7 +36,7 @@ function serveStaticFile(req,res){
             res.end()
         })
 
-    } if(req.url === '/doLogin' && req.method == 'POST'){
+    } else if(req.url === '/doLogin' && req.method == 'POST'){
         var loginData = ''
         req.on('data', chunk=>{
             loginData += chunk
@@ -47,7 +46,9 @@ function serveStaticFile(req,res){
             userDb.userData.forEach(ele=>{
                 if(ele.userID === loginObj.userID && ele.password === loginObj.password){
                     console.log(ele.userID)
+                    res.setHeader('Content-type','text/html')
                     res.write(`Welcome ${ele.userID}`)
+                    res.write('<br><a href="updateUserInfo.html">Update Profile</a><br><a href="updateUserInfo.html">Delete Account</a><br><a href="login.html">Logout</a>')
                     res.end()
                 } else {
                     res.write('Invalid Login ID and password')
@@ -59,6 +60,8 @@ function serveStaticFile(req,res){
             res.write(err)
             res.end()
         })
+    } else if(req.url === '/updateUserInfo.html'){
+        serveStaticFile(req.url,res)
     }
 }
 
