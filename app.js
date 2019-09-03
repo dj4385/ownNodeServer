aa8910bdf7d896801faa765bafd32fd2d8dd0a91
dheeraj sharma <dj4385@gmail.com>
2019-09-03 11:34:10 +0530

    delete func in process

--- app.js
+++ app.js
@@ -54,11 +54,11 @@ function serveStaticFile(req,res){
                     if(ele.userID === loginObj.userID && ele.password === loginObj.password){
                         console.log(ele.userID)
                         res.write(`Welcome ${ele.userID}`)
-                        res.write('<br><input type="button" value="Delete Account" id="del" onclick="deleteAcc()"><br><a href="login.html">Logout</a>')
+                        res.write('<br><a href="login.html">Logout</a>')
                         res.write(`<br><h3>Update Profiel</h3><br>
                         <form action="/doUpdate" method="POST">
                         <label for="">User ID</label><br>
-                        <input type="text" name="userID" id="" value="${ele.userID}" readonly><br>
+                        <input type="text" name="userID" id="uid" value="${ele.userID}" readonly><br>
                         <label for="">Password</label><br>
                         <input type="password" name="password" id="" value="${ele.password}"><br>
                         <label for="">Full Name</label><br>
@@ -69,14 +69,16 @@ function serveStaticFile(req,res){
                         <input type="text" name="phoneNo" id="" value="${ele.phoneNo}"><br>
                         <input type="submit" value="Update"/><br>
                     </form>
-    
+                    <br><input type="button" value="Delete Account" id="del" onclick="deleteAcc()" />
                     <script>
                         function deleteAcc(){
-                            let pr = fetch('http://localhost:8000/deleteAccount')
+                            //var uID = document.getElementById('uid').value
+                            var url = 'http://localhost:8000/deleteAccount'
+                            let pr = fetch(url)
                             pr.then((res,rej)=>{
-                               if(res){
-                                 alert(res)
-                               } 
+                               return res.json() 
+                            }).then(myjson=>{
+                                console.log(JSON.stringfy(myjson))
                             }).catch(err=>{
                                 alert(err)
                             })
@@ -98,10 +100,15 @@ function serveStaticFile(req,res){
             res.write(err)
             res.end()
         })
-    } else if(req.url === '/deleteAccount', req.method == 'DELETE'){
+    } else if(req.url.indexOf('/deleteAccount')>=0 && req.method == 'GET'){
+        var delAccData = url.parse(req.url,true)
+        
+        
         userDb.user.forEach(ele=>{
-            res.write(ele)
-            res.end()
+            if(ele.userID == delAccData.userID){
+                res.write(ele)
+                res.end()
+            }
         })
     } else if(req.url === '/doUpdate' && req.method=='POST'){
         var updateData = ''
